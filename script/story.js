3800f8bfa5a640ed8e28319cf510f1c627c897b6
FatOtter <46322760+FatOtter@users.noreply.github.com>
2022-01-22 00:47:40 +0800

    Update the calendar control

--- script/story.js
+++ script/story.js
@@ -1,3 +1,28 @@
+function get_date_string() {
+    var d = new Date();
+    var month = d.getMonth()+1;
+    var day = d.getDate();
+
+    var output = d.getFullYear() + '-' +
+    (month<10 ? '0' : '') + month + '-' +
+    (day<10 ? '0' : '') + day;
+    return output
+}
+
+function update_date_counter() {
+    var start_date = Date.parse("2016-10-17");
+    var end_string = $("#calendar").text();
+    var end_date = Date.parse(end_string);
+    var diff = Math.floor((end_date-start_date)/(24*3600*1000));
+    if (diff < 0) {
+        $("#date_comp").text("before");
+        $("#date_counter").text(-diff);
+    } else {
+        $("#date_comp").text("since");
+        $("#date_counter").text(diff);
+    }
+}
+
 $(document).ready(function(){
     var header_bottom = $("#header").height()
 
@@ -13,12 +38,22 @@ $(document).ready(function(){
         //control each content block
         $(".content_block").each(function(){
             var current = $(this)
-            var to_load = (current.position().top - scroll_top) / header_bottom
-            if (to_load < 1) {
-                current.css("opacity", to_load);
-                // console.log(current.css("opacity"));
+            // Handle fade out vision effect
+            var temp = (current.position().top - scroll_top) / header_bottom
+            if (temp < 1) {
+                current.css("opacity", temp);
             }
-        })
+
+            // Update calendar
+            var date_caption = current.find(".time_stamp").first().text();
+            temp = (current.position().top - scroll_top) / ($(window).height() / 2);
+            if (temp < 1) {
+                $("#calendar").text(date_caption);
+                console.log(date_caption)
+            }
+        });
+        update_date_counter();
     });
 
+    update_date_counter();
 });
